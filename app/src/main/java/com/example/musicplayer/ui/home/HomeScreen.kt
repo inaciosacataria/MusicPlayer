a93d07929a1fa24f2f42e75cd003d7e77a94895f
nebulaco <54165291+inaciosacataria@users.noreply.github.com>
2024-12-14 13:26:58 +0200

    update

--- app/src/main/java/com/example/musicplayer/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/musicplayer/ui/home/HomeScreen.kt
@@ -1,14 +1,18 @@
 package com.example.musicplayer.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
@@ -21,7 +25,9 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.musicplayer.R
 import com.example.musicplayer.ui.home.component.HomeAppBar
 import com.example.musicplayer.ui.home.component.MusicItem
 
@@ -43,6 +49,11 @@ fun HomeScreen(
                 backgroundColor = appBarColor,
                 modifier = Modifier.fillMaxWidth()
             )
+
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center){
+                Image(painter = painterResource(R.drawable.banner), contentDescription = null, modifier = Modifier.height(120.dp))
+            }
+
 
             with(uiState) {
                 when {
--- app/src/main/java/com/example/musicplayer/ui/home/component/HomeAppBar.kt
+++ app/src/main/java/com/example/musicplayer/ui/home/component/HomeAppBar.kt
@@ -1,17 +1,23 @@
 package com.example.musicplayer.ui.home.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.musicplayer.R
+
 
 @Composable
 fun HomeAppBar(
@@ -20,11 +26,12 @@ fun HomeAppBar(
 ) {
     TopAppBar(
         title = {
-            Row {
+            Row (verticalAlignment = Alignment.CenterVertically){
+                Image(painter = painterResource(R.drawable.letter), contentDescription = null, modifier = Modifier.size(30.dp))
                 Text(
                     modifier = Modifier
                         .padding(start = 8.dp),
-                    text = "Music Player"
+                    text = "Libertas Play"
                 )
             }
         },
